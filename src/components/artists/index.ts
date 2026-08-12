5b3df81b3a6e37dde39471989dc2bcfe4db55f1e
chinmaywadhwa007 <201734343+chinmaywadhwa007@users.noreply.github.com>
2026-08-12 15:22:49 +0530

    Delete src directory

--- src/components/artists/index.ts
+++
@@ -1,2 +0,0 @@
-export { ArtistCard } from './ArtistCard';
-export { FeaturedArtists } from './FeaturedArtists';
--- src/components/music/index.ts
+++
@@ -1,2 +0,0 @@
-export { AlbumCard } from './AlbumCard';
-export { LatestReleases } from './LatestReleases';
